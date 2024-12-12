1f2c1d7f8c1ad7756eba8da5576444291b2fa0c3
XandeBarros <xandebarrosdev@gmail.com>
2024-12-12 13:25:46 +0000

    Adicionando argumentos para escolha da arquitetura do Build

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -3,14 +3,16 @@
 # Valores padrão
 FOLDER_NAME="projeto"  # Nome padrão da pasta
 CLEAN_INSTALL=1        # Limpeza ativada por padrão
+IS_ARM=1               # Seleciona Arm32 como padrão
 
 # Função para exibir ajuda
 show_help() {
-  echo "Uso: build_and_run.sh [--folder-name=NOME_DA_PASTA] [--clean-install=0|1]"
+  echo "Uso: build_and_run.sh [--folder-name=NOME_DA_PASTA] [--clean-install=0|1] [--arm-target=0|1]"
   echo ""
   echo "Opções:"
   echo "  --folder-name=NOME_DA_PASTA   Define o nome da pasta onde o repositório será clonado. Padrão: 'projeto'."
   echo "  --clean-install=0|1           Define se os arquivos temporários serão excluídos (1) ou não (0). Padrão: 1."
+  echo "  --arm-target=0|1              Escolhe arquitetura do Build (1) ARM32 ou (0) X86_64. Padrão: 1 ARM32"
   exit 1
 }
 
@@ -22,6 +24,9 @@ for ARG in "$@"; do
       ;;
     --clean-install=*)
       CLEAN_INSTALL="${ARG#*=}"
+      ;;
+    --arm-target=*)
+      IS_ARM="${ARG#*=}"
       ;;
     --help|-h)
       show_help
@@ -68,7 +73,7 @@ docker exec "$CONTAINER_ID" chmod +x /eesc-aero/scripts/build_in_docker.sh
 
 # Executar o script de build dentro do contêiner
 echo "Executando o script de build dentro do contêiner..."
-docker exec "$CONTAINER_ID" /eesc-aero/scripts/build_in_docker.sh
+docker exec "$CONTAINER_ID" /eesc-aero/scripts/build_in_docker.sh --arm-target=IS_ARM
 
 # Finalizar e remover o contêiner após o processo
 echo "Finalizando o contêiner..."
--- scripts/build_in_docker.sh
+++ scripts/build_in_docker.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Função para exibir ajuda
+show_help() {
+  echo "Uso: build_in_docker.sh [--arm-target=0|1]"
+  echo ""
+  echo "Opções:"
+  echo "  --arm-target=0|1              Escolhe arquitetura do Build (1) ARM32 ou (0) X86_64. Padrão: 1 ARM32"
+  exit 1
+}
+
+# Parsear argumentos
+for ARG in "$@"; do
+  case $ARG in
+    --arm-target=*)
+      IS_ARM="${ARG#*=}"
+      ;;
+    --help|-h)
+      show_help
+      ;;
+    *)
+      echo "Opção desconhecida: $ARG"
+      show_help
+      ;;
+  esac
+done
+
 # Parar o script em caso de erro
 set -e
 
@@ -10,7 +35,7 @@ mkdir build-arm32
 cd build-arm32
 
 echo "Executando CMake com o ARM_TARGET..."
-cmake -DARM_TARGET=1 ..
+cmake -DARM_TARGET=IS_ARM ..
 
 echo "Construindo o programa com make..."
 make
