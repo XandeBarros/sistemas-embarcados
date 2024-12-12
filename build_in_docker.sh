8021ddae6949bfeedba6f06f3cbf692ee2ea7062
XandeBarros <xandebarrosdev@gmail.com>
2024-12-12 03:25:21 +0000

    Criando Bash para realizar os builds de forma automática

---
+++ build_in_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Parar o script em caso de erro
+set -e
+
+echo "Entrando no diretório do projeto..."
+cd eesc-aero
+
+echo "Criando diretório de build para ARM32..."
+mkdir -p build-arm32
+cd build-arm32
+
+echo "Executando CMake com o ARM_TARGET..."
+cmake -DARM_TARGET=1 ..
+
+echo "Construindo o programa com make..."
+make
+
+echo "Build concluído com sucesso!"
